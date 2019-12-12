a0cbde9d7dd3c4cd9094860c39223f0f59e7c6f2
lulusnack <45396092+lulusnack@users.noreply.github.com>
2019-12-13 00:22:43 +0800

    Add files via upload

---
+++ 10929.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	string input;
+	while (cin >> input)
+	{
+		int sum = 0;
+		int len = input.length();
+		if (input == "0")
+			break;
+		for (int i = 0; i < len; i++)
+		{
+			if (i % 2 == 0)
+				sum += (int)(input[i] - '0');
+			else
+				sum -= (int)(input[i] - '0');
+		}
+		sum = abs(sum);
+		if (sum % 11 == 0)
+			cout << input << " is a multiple of 11." << endl;
+		else
+			cout << input << " is not a multiple of 11." << endl;
+	}
+	return 0;
+}
